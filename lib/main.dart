dd00c949d6c26ba90b49267169d2d97d532984e9
bp18052 <bp18052@shibaura-it.ac.jp>
2022-01-21 13:53:49 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 73),
+              child: WebsafeSvg.asset(
+                'assets/ArcheryScoreSeet.svg',
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              child: TargetWidget(),
+            ),
+          ],
+        )),
+        backgroundColor: Color(0xffF2F5FA),
+      ),
+    );
+  }
+}
+
+class TargetWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: <Widget>[
+        Container(
+          width: 50.68,
+          height: 107,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.elliptical(50.68, 107)),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10.0,
+                  spreadRadius: 1.0,
+                  offset: Offset(10, 10)),
+            ],
+          ),
+        ),
+        Container(
+          width: 40.55,
+          height: 85.6,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.elliptical(40.55, 85.6)),
+            color: Color(0XFF4E4E4E),
+          ),
+        ),
+        Container(
+          width: 30.41,
+          height: 64.2,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.elliptical(30.41, 64.2)),
+            color: Color(0xff5f81f9),
+          ),
+        ),
+        Container(
+          width: 20.27,
+          height: 42.8,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.elliptical(20.27, 42.8)),
+            color: Color(0xfff95757),
+          ),
+        ),
+        Container(
+          width: 10.14,
+          height: 21.4,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.elliptical(10.14, 21.4)),
+            color: Color(0xfff6f95f),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
